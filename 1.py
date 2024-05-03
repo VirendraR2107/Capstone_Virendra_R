0dbed9d430668e5728d45f465a883b46bf62db4f
VirendraR2107 <160562981+VirendraR2107@users.noreply.github.com>
2024-05-03 16:43:42 +0530

    1.py

--- 1.py
+++ 1.py
@@ -1,73 +1,41 @@
 import streamlit as st
 import pickle
-import numpy as np
-import pandas as pd
 
+# Load the machine learning model and dataframe
 RF = pickle.load(open('RF_final_03-05-2024.pkl','rb')) 
 df1 = pickle.load(open('df1_03-05-2024.pkl','rb'))
 
+# Define the app title and header
 st.title('Used Car Price Prediction')
 st.header('Fill The Used Car Details to Predict The Price')
 
 # User inputs
-Car_Name=st.selectbox('Car_Name', df1['Car_Name'].unique())
-Model=st.selectbox('Model', df1['Model'].unique())
-Type=st.selectbox('Type', df1['Type'].unique())
-year=st.selectbox('year', df1['year'].unique())
-km_driven=st.number_input('KM(between 1.0 - 806599.0)')
-fuel=st.selectbox('fuel', df1['fuel'].unique())
-seller_type=st.selectbox('seller_type', df1['seller_type'].unique())
-transmission=st.selectbox('transmission', df1['transmission'].unique())
-owner=st.selectbox('owner', df1['owner'].unique())
+Car_Name = st.selectbox('Car_Name', df1['Car_Name'].unique())
+Model = st.selectbox('Model', df1['Model'].unique())
+Type = st.selectbox('Type', df1['Type'].unique())
 
-
-
-
-if st.button('Car_Name'):
-    if Car_Name == 'Maruti':
-        Car_Name=5
-    elif Car_Name == 'Hyundai':
-        Car_Name=3
-    elif Car_Name == 'Mahindra':
-        Car_Name=4
-    elif Car_Name=='Tata':
-        Car_Name=10
-    elif Car_Name=='Ford':
-        Car_Name=1
-    elif Car_Name=='Honda':
-        Car_Name=2
-    elif Car_Name=='Other':
-        Car_Name=7
-    elif Car_Name=='Toyota':
-        Car_Name=11
-    elif Car_Name=='Chevrolet':
-        Car_Name=0
-    elif Car_Name=='Renault':
-        Car_Name=8
-    elif Car_Name=='Volkswagen':
-        Car_Name=12
-    elif Car_Name=='Nissan':
-        Car_Name=6
-    elif Car_Name=='Skoda':
-        Car_Name=9
+# Mapping function for encoding categorical variables
+def encode_input(input_value, mapping_dict):
+    if input_value in mapping_dict:
+        return mapping_dict[input_value]
     else:
-        Car_Name ='Enter Correct Name'
+        return 'Enter Correct Value'
 
-
-
-
+# Button for prediction
 if st.button('Predict'):
-    # Encoding the selected values
-    encoded_car_name = encode_car_name(Car_Name)
-    encoded_model = encode_model(Model)
-    # Encode other columns similarly
+    # Encoding categorical inputs
+    Car_Name = encode_input(Car_Name, {'Maruti': 5, 'Hyundai': 3, 'Mahindra': 4, 'Tata': 10, 'Ford': 1,
+                                       'Honda': 2, 'Other': 7, 'Toyota': 11, 'Chevrolet': 0, 'Renault': 8,
+                                       'Volkswagen': 12, 'Nissan': 6, 'Skoda': 9})
+    Model = encode_input(Model, {'Other': 13, 'Swift': 16, 'Alto': 1, 'Wagon': 18, 'i20': 22,
+                                 'Verna': 17, 'Innova': 11, 'Indica': 9, 'Scorpio': 15, 'Santro': 14,
+                                 'Figo': 7, 'City': 4, 'EON': 5, 'Grand': 8, 'XUV500': 19, 'i10': 21,
+                                 'Bolero': 3, 'Beat': 2, 'KWID': 12, 'Ertiga': 6, 'Zen': 20, '800': 0,
+                                 'Indigo': 10})
+    Type = encode_input(Type, {'Other': 7, 'Dzire VDI': 2, 'LX': 5, '800 LXI': 0, 'LXi': 6,
+                               'VDI': 37, 'Era Plus': 3, 'VDI BSIV': 11, 'R VXI BS IV': 9, 'VXI': 13,
+                               'VDi': 12, 'R LXI Minor': 8, 'AC': 1, 'Xing GLS': 14, 'K10 VXI': 4})
 
-    # Construct input data array
-    input_data = np.array([encoded_car_name, encoded_model, Type, year, km_driven, fuel, seller_type, transmission, owner]).reshape(1, -1)
-    # Predict using the trained model
-    prediction = RF.predict(input_data)
-    st.write('The predicted price of the used car is:', prediction[0])
-
-
-
+    # Perform prediction with encoded inputs using the RF model
+    # ...
 
