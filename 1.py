f4565eec4d2aa0ff59f9a0a1e7da164e3807c096
VirendraR2107 <160562981+VirendraR2107@users.noreply.github.com>
2024-05-03 22:15:09 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import numpy as np
-import pandas as pd  # Import pandas for data manipulation
 
 # Load the trained model and dataframe
 RF = pickle.load(open('RF_final_03-05-2024.pkl','rb')) 
@@ -12,22 +11,17 @@ st.title('Used Car Price Prediction')
 st.header('Fill The Used Car Details to Predict The Price')
 
 # User inputs
-Car_Name = st.selectbox('Car_Name', df1['Car_Name'].unique())
-Model = st.selectbox('Model', df1['Model'].unique())
-Type = st.selectbox('Type', df1['Type'].unique())
+Car_Name=st.selectbox('Car_Name', df1['Car_Name'].unique())
+Model=st.selectbox('Model', df1['Model'].unique())
+Type=st.selectbox('Type', df1['Type'].unique())
+year=st.selectbox('year', df1['year'].unique())
+km_driven=st.number_input('KM(between 1.0 - 806599.0)')
+fuel=st.selectbox('fuel', df1['fuel'].unique())
+seller_type=st.selectbox('seller_type', df1['seller_type'].unique())
+transmission=st.selectbox('transmission', df1['transmission'].unique())
+owner=st.selectbox('owner', df1['owner'].unique())
 
-# Convert 'year' column to integers if possible
-try:
-    df1['year'] = df1['year'].astype(int)
-except ValueError:
-    pass  # If conversion to integer fails, keep it as is
 
-year = st.selectbox('year', df1['year'].unique())
-km_driven = st.number_input('KM(between 1.0 - 806599.0)')
-fuel = st.selectbox('fuel', df1['fuel'].unique())
-seller_type = st.selectbox('seller_type', df1['seller_type'].unique())
-transmission = st.selectbox('transmission', df1['transmission'].unique())
-owner = st.selectbox('owner', df1['owner'].unique())
 
 # Predict button
 if st.button('Predict'):
@@ -40,6 +34,11 @@ if st.button('Predict'):
         Car_Name = car_name_map.get(Car_Name, 'Enter Correct Car_Name')
         Model = model_map.get(Model, 'Enter Correct Model')
         Type = type_map.get(Type, 'Enter Correct Type')
+        
+        try:
+            year = int(year) if year.isdigit() and int(year) in df1['year'].unique() else 'Select Valid Year'
+        except ValueError:
+            year = 'Select Valid Year'
         
         fuel_map = {'Diesel': 1, 'Petrol': 4, 'CNG': 0, 'LPG': 3, 'Electric': 2}
         seller_type_map = {'Individual': 1, 'Dealer': 0, 'Trustmark Dealer': 2}
@@ -64,3 +63,6 @@ if st.button('Predict'):
             st.write("Please enter valid values for all input fields.")
     except Exception as e:
         st.write("An error occurred:", e)
+
+
+        
